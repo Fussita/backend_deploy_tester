a8fb7a23dd7125d5d87bbc892bffdd36c2b8fda8
Fussita <hualong.chiang@gmail.com>
2024-04-29 18:36:06 -0400

    serve

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
