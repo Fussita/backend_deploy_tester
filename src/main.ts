160e8bd43307f8569e2e34ab7eb42bdfe9011ef3
Fussita <hualong.chiang@gmail.com>
2024-04-29 18:42:48 -0400

    cors

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,7 @@ import { AppModule } from './app.module';
 import { login } from './auth/infraestructure/adapters/firebase/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  //const app = await NestFactory.create(AppModule, {cors: true});
-  app.enableCors()
+  const app = await NestFactory.create(AppModule, {cors: true});
   await app.listen(8080)
   login()
 }
